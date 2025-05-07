648b3c221970ce166d4286e9adb5106ba6d291fe
Albert Ferguson <albertferguson118@gmail.com>
2025-05-08 03:35:18 +1000

    configured Vite for Typescript library building

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite';
 import tsconfig from './tsconfig.app.json';
 import path from 'path';
+import packageJson from './package.json';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
+    plugins: [
+        // https://www.npmjs.com/package/vite-plugin-dts
+        dts({
+            rollupTypes: true,
+            tsconfigPath: './tsconfig.app.json',
+        }),
+    ],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: packageJson.name,
+            formats: ['es'],
+            fileName: packageJson.name,
+            cssFileName: packageJson.name,
+        },
+    },
     resolve: {
         alias: Object.fromEntries(
             Object.entries(tsconfig.compilerOptions.paths).map(
