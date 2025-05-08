41ec793dc5850f074bb7cb2dbcc83780a1b91fb9
Albert Ferguson <albertferguson118@gmail.com>
2025-05-09 03:49:41 +1000

    bump @easy-smart-app-banner versions for demo

--- demo/vue/src/main.ts
+++ demo/vue/src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 // import { default as createSmartAppBanner } from './smartAppBannerPlugin';
-import { type SmartBannerOptions } from '@easy-smart-app-banner/core';
+// import { type SmartBannerOptions } from '@easy-smart-app-banner/core';
 
 createApp(App)
     .use(createSmartAppBanner, {
