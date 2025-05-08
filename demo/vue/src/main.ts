4f17237780be0e6b8a15105d5a5f12d51e973991
Albert Ferguson <albertferguson118@gmail.com>
2025-05-09 03:55:19 +1000

    added demo code to demo

--- demo/vue/src/main.ts
+++ demo/vue/src/main.ts
@@ -2,15 +2,16 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
-// import { default as createSmartAppBanner } from './smartAppBannerPlugin';
-// import { type SmartBannerOptions } from '@easy-smart-app-banner/core';
+import { createSmartAppBanner } from '@easy-smart-app-banner/vue-plugin';
+import { type SmartBannerOptions } from '@easy-smart-app-banner/core';
 
 createApp(App)
     .use(createSmartAppBanner, {
         title: 'Hello world!',
         author: 'Albert Ferguson',
-        // using the create-vue asset logo
+        // using the create-vue asset logo (any image is valid!)
         icon: '/src/assets/logo.svg',
+        // optionally enable verbose logging - this is disabled by default
         verbose: true,
         googlePlayStoreUrl: 'https://play-store-application-url',
         appStoreUrl: 'https://app-store-application-url',
